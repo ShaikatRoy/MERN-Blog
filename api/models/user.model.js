e468d1721988ed589ce820569ad085ead97e569a
Shaikat roy <shaikatroy257@gmail.com>
2024-02-11 21:09:35 +0600

    create signup api route

--- api/models/user.model.js
+++ api/models/user.model.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 const userSchema = new mongoose.Schema({
     username: {
-        type: string,
+        type: String,
         required: true,
         unique: true,
     },
