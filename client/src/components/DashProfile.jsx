5d2d249288966455740b36109258b87f8c278757
Shaikat roy <shaikatroy257@gmail.com>
2024-02-18 01:47:29 +0600

    Add image upload feature to user profile page

--- client/src/components/DashProfile.jsx
+++ client/src/components/DashProfile.jsx
@@ -1,14 +1,48 @@
 import { Button, TextInput } from 'flowbite-react';
+import { useEffect, useRef, useState } from 'react';
 import { useSelector } from 'react-redux';
+import { getStorage, uploadBytesResumable } from 'firebase/storage'
+import { app } from '../firebase';
 
 export default function DashProfile() {
-    const {currentUser} = useSelector(state => state.user)
-  return (
+    const { currentUser } = useSelector(state => state.user);
+    const [imageFile, setImageFile] = useState(null);
+    const [imageFileUrl, setImageFileUrl] = useState(null);
+    const filePickerRef = useRef();
+    
+    const handleImageChange = (e) => {
+        const file = e.target.files[0];
+        if (file) {
+            setImageFile(file);
+            setImageFileUrl(URL.createObjectURL(file));
+        }
+    };
+    useEffect(() => {
+        if (imageFile) {
+            uploadImage();
+        }
+    }, [imageFile]);
+  
+    const uploadImage = async () => {
+        const storage = getStorage(app);
+        const fileName = new Date().getTime() + imageFile.name;
+        const storageRef = ref(storege, fileName);
+        const uploadTask = uploadBytesResumable(storageRef, imageFile);
+    };
+    
+    return (
     <div className='max-w-lg mx-auto p-3 w-full'>
         <h1 className='my-7 text-center font-semibold text-3xl'>Profile</h1>
         <form className='flex flex-col gap-4'>
-            <div className='w-32 h-32 self-center cursor-pointer shadow-md overflow-hidden rounded-full'>
-                <img src={currentUser.profilePicture} alt="user"
+            <input type='file' accept='image/*' 
+                    onChange={handleImageChange} 
+                    ref={filePickerRef}
+                    hidden        
+            />
+            <div className='w-32 h-32 self-center cursor-pointer shadow-md overflow-hidden rounded-full'
+                onClick={() => filePickerRef.current.click()}
+            >
+                <img src={imageFileUrl || currentUser.profilePicture} alt="user"
                     className='rounded-full w-full h-full object-cover border-8 border-[lightgray]'
                 />
             </div>
