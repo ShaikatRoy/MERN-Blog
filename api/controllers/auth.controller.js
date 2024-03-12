01a379ee48f2736599a7bc937f3e1d44ed62552e
Shaikat roy <shaikatroy257@gmail.com>
2024-03-12 14:01:11 +0600

    add admin functionality to the user

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -44,7 +44,7 @@ export const signin = async (req, res, next) => {
        return next(errorHandler(400, 'Invalid password'));
       }
       const token = jwt.sign(
-        { id: validUser._id },
+        { id: validUser._id, isAdmin:validUser.isAdmin },
         process.env.JWT_SECRET
       );
         
@@ -66,7 +66,7 @@ export const signin = async (req, res, next) => {
     try {
       const user = await User.findOne({email});
       if (user) {
-        const token = jwt.sign({id: user.id}, process.env.JWT_SECRET);
+        const token = jwt.sign({id: user.id, isAdmin: user.isAdmin }, process.env.JWT_SECRET);
         const {password, ...rest} = user._doc;
         res.status(200).cookie('access_token', token, {
           httpOnly: true,
--- api/models/user.model.js
+++ api/models/user.model.js
@@ -20,6 +20,10 @@ const userSchema = new mongoose.Schema({
         default: 
         'https://static.vecteezy.com/system/resources/thumbnails/009/292/244/small/default-avatar-icon-of-social-media-user-vector.jpg',
     },
+    isAdmin: {
+        type: Boolean,
+        default: false,
+    },
     }, {timestamps: true}
 
 );
